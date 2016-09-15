cc135fc59c3fce86638f26d628c6a3637b7cb83f
Vadim <vad.rostov@gmail.com>
2016-09-15 16:01:28 +0300

    wordc refactor

---
+++ src/main/java/glassclass/com/vadimrostov/glassclass/MailV/MailVFrame.java
@@ -0,0 +1,7 @@
+package main.java.glassclass.com.vadimrostov.glassclass.MailV;
+
+/**
+ * Created by Vad on 15.09.2016.
+ */
+public class MailVFrame {
+}
---
+++ src/main/java/glassclass/com/vadimrostov/glassclass/WordC/WordCDataHandler.java
@@ -0,0 +1,7 @@
+package main.java.glassclass.com.vadimrostov.glassclass.WordC;
+
+/**
+ * Created by Vad on 15.09.2016.
+ */
+public class WordCDataHandler {
+}
